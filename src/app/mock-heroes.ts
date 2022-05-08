4717e4f8d80fa3ec31cac0e06176983f0ee7dbf6
MinXin Su <minxinsu@MinXindeMacBook-Pro.local>
2022-05-09 00:46:36 +0800

    version 1

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr. Nick'},
+  { id: 12, name: 'Marco'},
+  { id: 13, name: 'Bomb'},
+  { id: 14, name: 'Celerita'},
+  { id: 15, name: 'Magneta'},
+  { id: 16, name: 'Dynamic'},
+  { id: 17, name: 'Tornado'},
+]
